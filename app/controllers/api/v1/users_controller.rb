56fd67ae0ccf40c726796bff5e3401f6902d2a77
choniyuazwan <choniyuazwan@gmail.com>
2021-12-08 23:00:20 +0700

    fix: create new user and response body book category

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ module Api
       def create
         @user = User.create(user_params)
         if @user.save
-          render json: UserRepresenter.new(user).as_json, status: :created
+          render json: UserRepresenter.new(@user).as_json, status: :created
         else
           render json: { error: @user.errors.full_messages.first }, status: :unprocessable_entity
         end
